e9625a91fbf7107a3922102f31cb1ed22e0d6024
ternicinc <allexander.linux@gmail.com>
2025-08-07 11:30:05 +0200

    FCORE

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,9 @@
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x90, 0x15, 0x69, 0x1c, 0x5f, 0xe5, 0x4d, 0x56, 0x8e, 0xb5, 0x7f, 0xab, 0x7b, 0x84, 0x17, 0xee } };
+}
